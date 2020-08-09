d7b84526c04e69c2cbf2cd6d74e58fe7306c563f
Jonathan <j.six@student.isartdigital.com>
2020-08-09 17:28:40 +0200

    Add vector 3 inherance to vector

---
+++ include/Vector/Vector3.hpp
@@ -0,0 +1,192 @@
+/*
+ * Project : FoxMath
+ * Editing by Six Jonathan
+ * Date : 2020-08-09 - 11 h 06
+ * 
+ * 
+ * MIT License
+ * 
+ * Copyright (c) 2020 Six Jonathan
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#pragma once
+
+#include "Vector.hpp"
+
+namespace FoxMath::Vector
+{
+    template <typename TType>
+    class Vector3 : public Vector<3, TType>
+    {
+        private:
+    
+        protected:
+    
+        #pragma region attribut
+        #pragma endregion //!attribut
+    
+        #pragma region static attribut
+        #pragma endregion //! static attribut
+    
+        #pragma region methods
+        #pragma endregion //!methods
+    
+        public:
+    
+        #pragma region constructor/destructor
+    
+        Vector3 ()					                = default;
+        Vector3 (const Vector3& other)			    = default;
+        Vector3 (Vector3&& other)				    = default;
+        virtual ~Vector3 ()				            = default;
+        Vector3& operator=(Vector3 const& other)    = default;
+        Vector3& operator=(Vector3 && other)		= default;
+
+        /**
+         * @brief Construct a new vector with it's base type
+         * 
+         * @tparam TTlengthOther 
+         * @param other 
+         */
+        template <size_t TTlengthOther>
+        Vector3 (const Vector<TTlengthOther, TType>& other)
+            : Vector<3, TType>(other) {}
+
+
+        /**
+         * @brief Variadic templated constructor to init member
+         * @example `FoxMath::Vector::Vector<3, int> vec (1, 1, 3)` or `FoxMath::Vector::Vector<3, int> vec (1, 3)`
+         * @tparam T 
+         * @tparam Sfinae IsSame 
+         */
+        template<typename... T, Type::IsSame<Type::Pack<TType, T...>, Type::Pack<T..., TType>> = true>
+        explicit inline constexpr
+        Vector3 (T... args) noexcept
+            : Vector<3, TType>(args...) {}
+
+        /**
+         * @brief Variadic templated constructor to init member with vector and scalar
+         * @example `FoxMath::Vector::Vector<10, int> vect(FoxMath::Vector::Vector<5, int>(), 3, 31)`
+         * @tparam TLengthOther 
+         * @tparam TScalarArgs
+         */
+        template<size_t TLengthOther, typename... TScalarArgs, 
+        Type::IsSame<Type::Pack<TType, TScalarArgs...>, Type::Pack<TScalarArgs..., TType>> = true,
+        Type::IsLessThanOrEqualTo<sizeof...(TScalarArgs) + TLengthOther, 3> = true,
+        Type::IsLessThan<TLengthOther, 3> = true>
+        explicit inline constexpr
+        Vector3 (const Vector<TLengthOther, TType>& other, TScalarArgs... args) noexcept
+            : Vector<3, TType>(other, args...) {}
+
+        #pragma endregion //!constructor/destructor
+    
+        #pragma region methods
+        #pragma endregion //!methods
+    
+        #pragma region accessor
+        #pragma endregion //!accessor
+    
+        #pragma region mutator
+        #pragma endregion //!mutator
+    
+        #pragma region operator
+
+        /**
+         * @brief simple assignment
+         * 
+         * @tparam TLengthOther 
+         * @tparam TType 
+         * @param other 
+         * @return constexpr Vector3& 
+         */
+        template <size_t TLengthOther, typename TTypeOther>
+		inline constexpr
+		Vector3& operator=(const Vector<TLengthOther, TTypeOther>& other) noexcept
+        {
+            Vector<3, TType>::operator=(other);
+            return *this;
+        }
+
+        /**
+         * @brief simple assignment
+         * 
+         * @tparam TLengthOther 
+         * @tparam TType 
+         * @param other 
+         * @return constexpr Vector3& 
+         */
+        template<typename TscalarType, Type::IsArithmetic<TscalarType> = true>
+		inline constexpr
+		Vector3& operator=(TscalarType scalar) noexcept
+        {
+            Vector<3, TType>::operator=(scalar);
+            return *this;
+        }
+
+        #pragma endregion //!operator
+    
+        #pragma region convertor
+
+        #pragma endregion //!convertor
+    
+        #pragma region static attribut
+
+        static constexpr inline Vector3 zero      = Vector3(static_cast<TType>(0), static_cast<TType>(0), static_cast<TType>(0));
+		static constexpr inline Vector3 one       = Vector3(static_cast<TType>(1), static_cast<TType>(1), static_cast<TType>(1));
+		static constexpr inline Vector3 up        = Vector3(static_cast<TType>(0), static_cast<TType>(1), static_cast<TType>(0));
+		static constexpr inline Vector3 down      = Vector3(static_cast<TType>(0), static_cast<TType>(-1), static_cast<TType>(0));
+		static constexpr inline Vector3 right     = Vector3(static_cast<TType>(1), static_cast<TType>(0), static_cast<TType>(0));
+		static constexpr inline Vector3 left      = Vector3(static_cast<TType>(-1), static_cast<TType>(0), static_cast<TType>(0));
+		static constexpr inline Vector3 forward   = Vector3(static_cast<TType>(0), static_cast<TType>(0), static_cast<TType>(1));
+		static constexpr inline Vector3 backward  = Vector3(static_cast<TType>(0), static_cast<TType>(0), static_cast<TType>(-1));
+
+        #pragma endregion //! static attribut
+    };
+
+    template <typename TType>
+    using Vec3          = Vector3<TType>;
+
+    using Vector3f      = Vector3<float>;
+    using Vec3f         = Vector3f;
+    using Vector3d      = Vector3<double>;
+    using Vec3d         = Vector3d;
+    using Vector3ld     = Vector3<long double>;
+    using Vec3ld        = Vector3ld;
+
+    using Vector3uc     = Vector3<unsigned char>;
+    using Vec3uc        = Vector3uc;
+    using Vector3c      = Vector3<char>;
+    using Vec3c         = Vector3c;
+    using Vector3ui     = Vector3<unsigned int>;
+    using Vec3ui        = Vector3ui;
+    using Vector3i      = Vector3<int>;
+    using Vec3i         = Vector3i;
+    using Vector3lui    = Vector3<long unsigned int>;
+    using Vec3lui       = Vector3lui;
+    using Vector3st     = Vector3<size_t>;
+    using Vec3st        = Vector3st;
+    using Vector3li     = Vector3<long int>;
+    using Vec3li        = Vector3li;
+    
+    using Vector3b      = Vector3<bool>;
+    using Vec3b         = Vector3b;
+
+} /*namespace FoxMath::Vector*/
