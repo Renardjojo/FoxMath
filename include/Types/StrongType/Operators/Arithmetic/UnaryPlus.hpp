174578cfacaa585d3fc376bf970b0752315f463d
Jonathan <j.six@student.isartdigital.com>
2020-08-16 15:14:14 +0200

    Add arithmetic operator

---
+++ include/Types/StrongType/Operators/Arithmetic/UnaryPlus.hpp
@@ -0,0 +1,64 @@
+/*
+ * Project : FoxMath
+ * Editing by Six Jonathan
+ * Date : 2020-08-16 - 12 h 37
+ * 
+ * 
+ * MIT License
+ * 
+ * Copyright (c) 2020 Six Jonathan
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#pragma once
+
+#include "Types/StrongType/StrongType/StrongType.hpp"
+
+namespace FoxMath::Type::StrongType::Operator::Arithmetic
+{
+    /**
+     * @brief UnaryPlus operator class
+     * 
+     * This class is meant to be used in conjunction with the NamedType class.
+     * This allows for better and quicker operator integrations to named types.
+     * 
+     * @tparam TStrongTypedef Base NamedType
+     *
+     * @see NamedType
+     */
+    template <typename TStrongTypedef>
+    struct UnaryPlus
+    {
+        /**
+         * @brief UnaryPlus operator
+         *
+         * @param in_instance Operand instance
+         *
+         * @return Value of the new instance
+         */
+        constexpr inline
+        TStrongTypedef operator+(TStrongTypedef const& in_instance) noexcept
+        {
+            using Type = UnderlyingType<TStrongTypedef>;
+
+            return TStrongTypedef(+static_cast<Type const&>(in_instance));
+        }
+    };
+} /*namespace FoxMath::Type::StrongType::Operator::Arithmetic*/
