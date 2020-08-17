b7608411faed6fd5fb3603de12c2924add9729f5
Jonathan <j.six@student.isartdigital.com>
2020-08-17 14:25:41 +0200

    Update main example

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@ int main()
 {
     /*Generic vector sandbox*/
     {
+        std::cout << std::endl << std::endl << "Generic vector sandbox" << std::endl;
         Vector<3, float> vect (3.f, 5.f, 10.f);
         constexpr Vector<3, float> constVect (3.f, 5.f, 10.f);
         constexpr Vector<3, float> constVect2  (5.f, 5.f);
@@ -34,12 +35,14 @@ int main()
         std::cout << vect.reflect(constVect2.getNormalized()) << std::endl;
         std::cout << value.getReflection(constVect2.getNormalized()) << std::endl;
         std::cout << vect.setLength(10) << std::endl;
+        std::cout << vect.homogenize() << std::endl;
+        std::cout << value.getHomogenized() << std::endl;
         std::cout << value.isColinearTo(constVect2) << std::endl;
         std::cout << value.isPerpendicularTo(constVect2) << std::endl;
         std::cout << value.getParallelogramArea(constVect2) << std::endl;
         std::cout << value.getTriangleArea(constVect2) << std::endl;
-        std::cout << vect.rotateAroundAxis(constVect2.getNormalized(), 3.f) << std::endl;
-        std::cout << value.getRotationAroundAxis(constVect2.getNormalized(), 3.f) << std::endl;
+        std::cout << vect.rotateAroundAxis(constVect2.getNormalized(), 3_rad) << std::endl;
+        std::cout << value.getRotationAroundAxis(constVect2.getNormalized(), 3_deg) << std::endl;
         std::cout << value.getScalarProjectionWith(constVect2) << std::endl;
         std::cout << value.getScalarRejectionWith(constVect2) << std::endl;
         std::cout << value.getVectorProjectionWith(constVect2) << std::endl;
@@ -48,6 +51,7 @@ int main()
 
     /*Compatibility sandbox*/
     {
+        std::cout << std::endl << std::endl << "Compatibility sandbox" << std::endl;
         Vec2f  vec2f (1.f, 2.f);
         Vec3f  vec3f(vec2f, 3.f);
         Vec4f  vec4f (vec3f, 4.f);
@@ -58,6 +62,7 @@ int main()
 
         /*constuctor*/
         {
+            std::cout << std::endl << std::endl << "constuctor" << std::endl;
             Vector<2, float>  baseVec2ff (vec2f);
             Vector<3, float>  baseVec3ff (vec3f);
             Vector<4, float>  baseVec4ff (vec4f);
@@ -81,6 +86,7 @@ int main()
     
         /*Static_cast*/
         {
+            std::cout << std::endl << std::endl << "Static_cast" << std::endl;
             Vector<2, float>  baseVec2ff (static_cast<Vector<2, float>>(vec2f));
             Vector<3, float>  baseVec3ff (static_cast<Vector<3, float>>(vec3f));
             Vector<4, float>  baseVec4ff (static_cast<Vector<4, float>>(vec4f));
@@ -104,6 +110,7 @@ int main()
 
         /*operator =*/
         {
+            std::cout << std::endl << std::endl << "operator =" << std::endl;
             Vector<2, float>  baseVec2ff = vec2f;
             Vector<3, float>  baseVec3ff = vec3f;
             Vector<4, float>  baseVec4ff = vec4f;
@@ -137,18 +144,35 @@ int main()
 
     /*Specifique Vector2 methode sandbox*/
     {
+        std::cout << std::endl << std::endl << "Specifique Vector2 methode sandbox" << std::endl;
 
+        Vec2i vec(0, 1);
+        Vec2i vecU(1, 0);
+
+        int a = static_cast<int>(3_deg);
+        std::cout << a << std::endl;
+
+        std::cout << vec.getRotationAroundAxis(vecU, 3_deg) << std::endl;
     }
 
     /*Angle sandbox*/
     {
+        std::cout << std::endl << std::endl << "Angle sandbox" << std::endl;
+
         int a = 5;
-        auto angle = 90_deg;
+        Angle<EAngleType::Degree, float> angle = 3_deg;
+        std::cout << angle << std::endl;
+        
         angle += 3.14_rad;
+        std::cout << angle << std::endl;
+        
         angle -= 3_rad;
+        std::cout << angle << std::endl;
+        
         angle += 50.6_deg;
+        std::cout << angle << std::endl;
+        
         angle += Angle<EAngleType::Radian, float>(a);
         std::cout << angle << std::endl;
-
     }
 }
