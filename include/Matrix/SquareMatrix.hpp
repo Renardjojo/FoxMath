8c88f9ff92e13bdf675eb8fb385b7c0797fedd6e
Jonathan <j.six@student.isartdigital.com>
2020-08-24 17:00:17 +0200

    Add square matrix class to avoid assert

---
+++ include/Matrix/SquareMatrix.hpp
@@ -0,0 +1,301 @@
+/*
+ * Project : FoxMath
+ * Editing by Six Jonathan
+ * Date : 2020-08-24 - 14 h 15
+ * 
+ * 
+ * MIT License
+ * 
+ * Copyright (c) 2020 Six Jonathan
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#pragma once
+
+#include "Matrix/GenericMatrix.hpp"
+
+namespace FoxMath::Matrix
+{
+    /*By inherance size cannot be equal to zero and TType must be arythmetic*/
+    template <size_t TSize, typename TType = float, EMatrixConvention TMatrixConvention = EMatrixConvention::ColumnMajor>
+    class SquareMatrix : public GenericMatrix<TSize, TSize, TType, TMatrixConvention>
+    {
+        private:
+    
+        protected:
+    
+        #pragma region attribut
+        #pragma endregion //!attribut
+    
+        #pragma region static attribut
+        #pragma endregion //! static attribut
+    
+        #pragma region methods
+
+        /**
+         * @brief return the determinant of minor element in function of M aij.
+         * 
+         * @param i : start to 0 just to n - 1 
+         * @param j : start to 0 just to n - 1
+         * @return constexpr TType 
+         */
+        [[nodiscard]] inline constexpr  
+        TType		getMinor		(size_t i, size_t j) const noexcept;
+
+        /**
+         * @brief return the result of coeficient multipliate by his cofactor multipliate by the signe : (-1)^(i+j) * minor(i, j)
+         * 
+         * @param i : start to 0 just to n - 1 
+         * @param j : start to 0 just to n - 1
+         * @return constexpr TType 
+         */
+        [[nodiscard]] inline constexpr  
+        TType		getCofactor		(size_t i, size_t j) const noexcept;
+            
+        /**
+         * @brief return the cofactor matrix of the current matrix.
+         * 
+         * @return constexpr SquareMatrix 
+         */
+        [[nodiscard]] inline constexpr  
+        SquareMatrix		getCoMatrix		() const noexcept;
+
+        /**
+         * @brief transpose the cofactor matrix.
+         * 
+         */
+        inline constexpr  
+        void		tranformCoMatToAdjointMat		() noexcept;
+
+        #pragma endregion //!methods
+    
+        public:
+    
+        #pragma region constructor/destructor
+    
+        constexpr inline
+        SquareMatrix () noexcept                                            = default;
+
+        constexpr inline
+        SquareMatrix (const SquareMatrix& other) noexcept			        = default;
+
+        constexpr inline
+        SquareMatrix (SquareMatrix&& other) noexcept				        = default;
+
+        inline
+        ~SquareMatrix () noexcept				                            = default;
+
+        constexpr inline
+        SquareMatrix& operator=(SquareMatrix const& other) noexcept			= default;
+
+        constexpr inline
+        SquareMatrix& operator=(SquareMatrix && other) noexcept			    = default;
+    
+        /**
+         * @brief Construct a new matrix with it's base type
+         * 
+         * @tparam TTlengthOther 
+         * @param other 
+         */
+        constexpr inline
+        SquareMatrix (const GenericMatrix<TSize, TSize, TType, TMatrixConvention>& other) noexcept
+            : GenericMatrix<TSize, TSize, TType, TMatrixConvention>(other)
+        {}
+
+        /**
+         * @brief Aggregate initialization for scalar
+         * 
+         * @note : If the number of initializer clauses is less than the number of members or initializer list is completely empty, the remaining members are value-initialized. 
+         * If a member of a reference type is one of these remaining members, the program is ill-formed.
+         * 
+         * @example `FoxMath::Matrix::SquareMatrix<2, int> mat (1, 1, 3)` or `FoxMath::Matrix::SquareMatrix<2, int> mat (1, 2)`
+         * 
+         * @tparam T 
+         * @tparam true 
+         * @tparam true 
+         */
+        template<typename... T, Type::IsSame<Type::Pack<TType, T...>, Type::Pack<T..., TType>> = true>
+        explicit inline constexpr
+        SquareMatrix (T... args) noexcept
+            : GenericMatrix<TSize, TSize, TType, TMatrixConvention>(args...)
+        {}
+
+        /**
+         * @brief Aggregate initialization for vector
+         * 
+         * @note : If the number of initializer clauses is less than the number of members or initializer list is completely empty, the remaining members are value-initialized. 
+         * If a member of a reference type is one of these remaining members, the program is ill-formed.
+         * 
+         * @example `FoxMath::Matrix::SquareMatrix<2, int> mat (Vec2f(1, 1), Vec2f(3))` or `FoxMath::Matrix::SquareMatrix<2, int> mat (Vec2f(1.f, 2.f))`
+         * 
+         * @tparam T 
+         * @tparam true 
+         * @tparam true 
+         */
+        template<typename... T>
+        explicit inline constexpr
+        SquareMatrix (T... args) noexcept
+            : GenericMatrix<TSize, TSize, TType, TMatrixConvention>(args...)
+        {}
+
+
+        #pragma endregion //!constructor/destructor
+    
+        #pragma region methods
+
+        /**
+         * @brief affect identity to matrix
+         * 
+         * @return constexpr SquareMatrix& 
+         */
+        inline constexpr  
+        SquareMatrix& generateIdentity () noexcept;
+
+        /**
+         * @brief Transpose the matrix on itself. Only square matrix can do that because of static matrix
+         * 
+         * @return constexpr SquareMatrix& 
+         */
+        inline constexpr  
+        SquareMatrix&		transpose		() noexcept;
+
+        /**
+         * @brief return true of false if matrix is orthogonal. If M*M.transpose() = I.
+         * 
+         * @return true 
+         * @return false 
+         */
+        [[nodiscard]] inline constexpr  
+        bool		isOrtho		() const noexcept;
+    
+        /**
+         * @brief Calcul the derteminant of square matrix X*X.
+         *		  If determinant is geometrical area betwen eache vector in matrix.
+         * 
+         * @return constexpr TType 
+         */
+        [[nodiscard]] inline constexpr  
+        TType		getDeterminant		() const noexcept;
+
+        /**
+         * @brief reserse matrix if it's possible, else return false. 
+         * If function if orthogonal, this function return the transposate of matrix.
+         * or empty
+         * 
+         * @return Matrix return empty matrix if reverse is not possible
+         */
+        [[nodiscard]] inline constexpr  
+        SquareMatrix		getReverse		() const noexcept;
+
+        /**
+         * @brief reserse matrix if it's possible, else return false.
+         * If function if orthogonal, matrix will be transpose.
+         * or empty
+         * 
+         */
+        inline constexpr 
+        void		reverse		() noexcept;
+
+        #pragma endregion //!methods
+
+        #pragma region static methods
+
+        /**
+         * @brief Return true if the number of column and row is same.
+         * 
+         * @return true
+         */
+        [[nodiscard]] static inline constexpr
+		bool 	    isSquare	() noexcept
+        {
+            return true;
+        }
+
+        /**
+         * @brief Get identity matrix.
+         * 
+         * @return constexpr SquareMatrix& 
+         */
+        [[nodiscard]] static inline constexpr  
+        GenericMatrix<TSize, TSize, TType, TMatrixConvention> identity () noexcept
+        {
+            assert (isSquare());
+
+            SquareMatrix rst;
+            rst.generateIdentity();
+            return rst;
+        }
+
+        #pragma endregion //!static methods
+
+        #pragma region accessor
+        #pragma endregion //!accessor
+    
+        #pragma region mutator
+        #pragma endregion //!mutator
+    
+        #pragma region operator
+
+        /**
+         * @brief Converte generic matrix to another generic matrix type
+         * @note use static_cast<> to call this function. Is use only if you try to downgrade generic matrix like static_cast<Vec3f>(vec4f). Else use the constructor
+         * @example `FoxMath::Matrix::Matrix<2, 3, float> rhs = static_cast<FoxMath::Matrix::Matrix<8, 2, int, EMatrixConvention::RowMajor>>(lhs)`
+         * 
+         * @tparam TRowSizeOther 
+         * @tparam TColumnSizeOther 
+         * @tparam TTypeOther 
+         * @tparam TMatrixConventionOther 
+         * @return GenericMatrix<TRowSizeOther, TColumnSizeOther, TTypeOther, TMatrixConvention> 
+         */
+        template <size_t TSizeOther, typename TTypeOther, EMatrixConvention TMatrixConventionOther>
+        [[nodiscard]] implicit constexpr inline
+        operator SquareMatrix<TSizeOther, TTypeOther, TMatrixConventionOther>() const noexcept
+        {
+            GenericMatrix<TSizeOther, TSizeOther, TTypeOther, TMatrixConventionOther> rstConv = (*this);
+            return rstConv;
+        }
+
+        /**
+         * @brief simple assignment
+         * 
+         * @tparam TSizeOther 
+         * @tparam TTypeOther 
+         * @tparam TMatrixConventionOther 
+         * @param other 
+         * @return constexpr SquareMatrix& 
+         */
+        template <size_t TSizeOther, typename TTypeOther, EMatrixConvention TMatrixConventionOther>
+		inline constexpr
+		SquareMatrix& operator=(const GenericMatrix<TSizeOther, TSizeOther, TTypeOther, TMatrixConventionOther>& other) noexcept
+        {
+            GenericMatrix<TSize, TSize, TType, TMatrixConvention>::operator=(other);
+            return *this;
+        }
+
+
+        #pragma endregion //!operator
+    
+        #pragma region convertor
+        #pragma endregion //!convertor
+    };
+
+    #include "Matrix/SquareMatrix.inl"
+
+} /*namespace FoxMath::Matrix*/
---
+++ include/Matrix/SquareMatrix.inl
@@ -0,0 +1,234 @@
+/*
+ * Project : FoxMath
+ * Editing by Six Jonathan
+ * Date : 2020-08-24 - 14 h 53
+ * 
+ * 
+ * MIT License
+ * 
+ * Copyright (c) 2020 Six Jonathan
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#pragma once
+
+template <size_t TSize, typename TType, EMatrixConvention TMatrixConvention>
+inline constexpr  
+TType		SquareMatrix<TSize, TType, TMatrixConvention>::getMinor		(size_t i, size_t j) const noexcept
+{
+    const size_t newSizesubMatrix = TSize - 1;
+	SquareMatrix<newSizesubMatrix, TType, TMatrixConvention> subMatrix;
+	//fill submatrix
+	bool coefLineFound = false;
+	bool coefRowFound = false;
+
+	for (size_t iSubMatrix = 0; iSubMatrix < newSizesubMatrix; iSubMatrix++)
+	{
+		if (iSubMatrix == i)
+			coefLineFound = true;
+
+		for (size_t jSubMatrix = 0; jSubMatrix < newSizesubMatrix; jSubMatrix++)
+		{
+			if (jSubMatrix == j)
+				coefRowFound = true;
+
+			subMatrix.getData(iSubMatrix * newSizesubMatrix + jSubMatrix) = GenericMatrix<TSize, TSize, TType, TMatrixConvention>::m_data[(iSubMatrix + coefLineFound) * TSize + jSubMatrix + coefRowFound];
+		}
+		coefRowFound = false;
+	}
+
+	return subMatrix.getDeterminant();
+}
+
+template <size_t TSize, typename TType, EMatrixConvention TMatrixConvention>
+inline constexpr  
+TType		SquareMatrix<TSize, TType, TMatrixConvention>::getCofactor		(size_t i, size_t j) const noexcept
+{
+	return std::pow(-1, i + j) * getMinor(i, j);
+}
+
+template <size_t TSize, typename TType, EMatrixConvention TMatrixConvention>
+inline constexpr  
+SquareMatrix<TSize, TType, TMatrixConvention>	SquareMatrix<TSize, TType, TMatrixConvention>::getCoMatrix		() const noexcept
+{
+	SquareMatrix<TSize, TType, TMatrixConvention> coMatrix;
+
+    for ( size_t i = 0; i < TSize; i++ )
+	{
+        for ( size_t j = 0; j < TSize; j++ )
+		{
+			coMatrix.getData(i * TSize + j) = getCofactor(i, j);
+		}
+	}
+	
+	return coMatrix;
+}
+
+template <size_t TSize, typename TType, EMatrixConvention TMatrixConvention>
+inline constexpr  
+void		SquareMatrix<TSize, TType, TMatrixConvention>::tranformCoMatToAdjointMat		() noexcept
+{
+	transpose();
+}
+
+
+template <size_t TSize, typename TType, EMatrixConvention TMatrixConvention>
+inline constexpr  
+SquareMatrix<TSize, TType, TMatrixConvention>& SquareMatrix<TSize, TType, TMatrixConvention>::generateIdentity () noexcept
+{
+    for ( size_t i = 0; i < TSize; i++ )
+    {
+        for ( size_t j = 0; j < TSize; j++ )
+        {
+            GenericMatrix<TSize, TSize, TType, TMatrixConvention>::m_data[i * TSize + j] = ((i == j) ? static_cast<TType>(1) : static_cast<TType>(0));
+        }
+    }
+
+    return *this;
+}
+
+
+template <size_t TSize, typename TType, EMatrixConvention TMatrixConvention>
+inline constexpr  
+SquareMatrix<TSize, TType, TMatrixConvention>&		SquareMatrix<TSize, TType, TMatrixConvention>::transpose		() noexcept
+{
+    size_t shift = 1;
+
+    for (size_t i = 0; i < TSize - 1; i++)
+    {
+        for (size_t j = shift; j < TSize; j++)
+        {
+#if __cplusplus >= 201709L //TODO: constexpr swap
+            std::swap(GenericMatrix<TSize, TSize, TType, TMatrixConvention>::m_data[i * TSize + j], GenericMatrix<TSize, TSize, TType, TMatrixConvention>::m_data[j * TSize + i]);
+#else
+            if constexpr (std::is_floating_point_v<TType>)
+            {
+                TType temp = GenericMatrix<TSize, TSize, TType, TMatrixConvention>::m_data[i * TSize + j];
+                GenericMatrix<TSize, TSize, TType, TMatrixConvention>::m_data[i * TSize + j] = GenericMatrix<TSize, TSize, TType, TMatrixConvention>::m_data[j * TSize + i];
+                GenericMatrix<TSize, TSize, TType, TMatrixConvention>::m_data[j * TSize + i] = temp;
+            }
+            else
+            {
+                GenericMatrix<TSize, TSize, TType, TMatrixConvention>::m_data[i * TSize + j] ^= GenericMatrix<TSize, TSize, TType, TMatrixConvention>::m_data[j * TSize + i];
+                GenericMatrix<TSize, TSize, TType, TMatrixConvention>::m_data[j * TSize + i] ^= GenericMatrix<TSize, TSize, TType, TMatrixConvention>::m_data[i * TSize + j];
+                GenericMatrix<TSize, TSize, TType, TMatrixConvention>::m_data[i * TSize + j] ^= GenericMatrix<TSize, TSize, TType, TMatrixConvention>::m_data[j * TSize + i];
+            }
+#endif
+        }
+        shift++;
+    }
+
+    
+    return *this;
+}
+
+template <size_t TSize, typename TType, EMatrixConvention TMatrixConvention>
+inline constexpr  
+bool		SquareMatrix<TSize, TType, TMatrixConvention>::isOrtho		() const noexcept
+{
+	const SquareMatrix<TSize, TType, TMatrixConvention> mT (GenericMatrix<TSize, TSize, TType, TMatrixConvention>::getTransposed());
+
+	return mT * (*this) == identity();
+}
+
+template <size_t TSize, typename TType, EMatrixConvention TMatrixConvention>
+inline constexpr  
+TType		SquareMatrix<TSize, TType, TMatrixConvention>::getDeterminant		() const noexcept
+{
+    if constexpr (TSize * TSize == 1) //Check if matrix 1*1
+    {
+        return GenericMatrix<TSize, TSize, TType, TMatrixConvention>::getData(0);
+    }
+    else
+    {
+        TType result = static_cast<TType>(0);
+        
+        for (size_t i = 0; i < TSize; i++)
+        {
+            //found signe
+            signed int signe = pow(-1, 0 + i);
+
+            //found coef
+            TType coef = signe * GenericMatrix<TSize, TSize, TType, TMatrixConvention>::m_data[i * TSize];
+        
+            //create submatrix
+            const size_t subMatrixSize = TSize - 1;
+            SquareMatrix<subMatrixSize, TType, TMatrixConvention> subMatrix;
+
+            //fill submatrix
+            bool coefLineFound = false;
+
+            for (size_t iSubMatrix = 0; iSubMatrix < subMatrixSize; iSubMatrix++)
+            {
+                if (iSubMatrix == i)
+                    coefLineFound = true;
+
+                for (size_t jSubMatrix = 0; jSubMatrix < subMatrixSize; jSubMatrix++)
+                { 
+                    subMatrix.getData(iSubMatrix * subMatrixSize + jSubMatrix) = GenericMatrix<TSize, TSize, TType, TMatrixConvention>::m_data[(iSubMatrix + coefLineFound) * TSize + jSubMatrix + 1];
+                }
+            }
+
+            //found determinant
+            TType determinant = subMatrix.getDeterminant();
+
+            //found result
+            result += coef * determinant;
+        }
+        
+        return result;
+    }
+}
+
+template <size_t TSize, typename TType, EMatrixConvention TMatrixConvention>
+inline constexpr  
+SquareMatrix<TSize, TType, TMatrixConvention>		SquareMatrix<TSize, TType, TMatrixConvention>::getReverse		() const noexcept
+{
+	assert ((*this) != static_cast<TType>(0));
+
+	if (isOrtho() == true)
+	{
+		return GenericMatrix<TSize, TSize, TType, TMatrixConvention>::getTransposed();
+	}
+
+	const TType determinant = getDeterminant();
+	
+	if (Numeric::isSameAsZero<TType>(determinant)) //in two step for more perform
+		return SquareMatrix<TSize, TType, TMatrixConvention>::zero();
+
+	SquareMatrix<TSize, TType, TMatrixConvention> reversedMatrix = getCoMatrix();
+
+	reversedMatrix.tranformCoMatToAdjointMat();
+
+	reversedMatrix /= determinant;
+
+	return reversedMatrix;
+}
+
+template <size_t TSize, typename TType, EMatrixConvention TMatrixConvention>
+inline constexpr 
+void		SquareMatrix<TSize, TType, TMatrixConvention>::reverse		() noexcept
+{
+	assert ((*this) != static_cast<TType>(0));
+
+	SquareMatrix<TSize, TType, TMatrixConvention> reversedMatrix = getReverse();
+
+	(*this) = reversedMatrix;
+}
