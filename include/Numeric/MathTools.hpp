5da63681ca094297a041f626745af5a65f9e3b26
Six Jonathan <Six-jonathan@orange.fr>
2021-09-01 08:47:24 +0200

    [Add] Ajout de unrollIf

--- include/Numeric/MathTools.hpp
+++ include/Numeric/MathTools.hpp
@@ -9,18 +9,24 @@
 
 namespace FoxMath
 {
-    /*isBetween(5, 0, 10)*/
+    /*isBetween(5, 0, 10) runtime*/
     template<class T = float>
     T isBetween(T number, T low, T up)
     {
         return number >= low && number <= up;
     }
 
-    /*isBetween<0, 10>(5)*/
+    /*isBetween<0, 10>(5) compile time*/
     template<typename T = float, T Min, T Max>
     auto isBetween(T value) -> std::enable_if_t<Min <= Max, bool>
     {
         return value >= Min && value <= Max;
+    }
+
+    template <typename T>
+    inline T unrolledIf(bool condition, T valIfTrue, T valIfFalse)
+    {
+        return condition * valIfTrue + !condition * valIfFalse;
     }
 }
 
